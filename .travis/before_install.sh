54321c15f0615182cadd365e2b7c8624d4dccfe3
Austin Heiman <atheimanksu@gmail.com>
2016-08-17 20:39:20 -0500

    travis updates

--- .travis/before_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo wget http://repo.zabbix.com/zabbix/2.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_2.2-1+trusty_all.deb
-sudo dpkg -i zabbix-release_2.2-1+trusty_all.deb
-sudo apt-get -q update
-sudo apt-get -qy install zabbix-server-mysql zabbix-frontend-php
--- .travis/before_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-bundle
-echo "before_script.sh pwd: $(pwd)"
-bundle exec ./.travis/create_zabbix_objects.rb
